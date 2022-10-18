8140d8152d68b45edb2939d8934ecc052678af15
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-10-18 04:37:33 +0200

    feat(playground): add module configuration

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -1,5 +1,38 @@
 export default defineNuxtConfig({
-  modules: ['@dargmuesli/nuxt-cookie-control'],
+  cookies: {
+    necessary: [
+      {
+        name: {
+          de: 'Notwendiger Cookie',
+          en: 'Necessary Cookie',
+        },
+        cookies: ['NEC'],
+      },
+    ],
+    optional: [
+      {
+        name: 'Optional Cookie',
+        identifier: 'op',
+        cookies: ['_o', '_p', '_t'],
+        accepted: () => {
+          // const app = useNuxtApp()
+          alert('accepted')
+        },
+        declined: () => {
+          // const app = useNuxtApp()
+          alert('declined')
+        },
+      },
+    ],
+  },
+  modules: [
+    [
+      '@dargmuesli/nuxt-cookie-control',
+      {
+        locales: ['en', 'de'],
+      },
+    ],
+  ],
   typescript: {
     tsConfig: {
       include: ['../../**/*'], // https://github.com/nuxt/framework/pull/7726
