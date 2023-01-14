189e9378a60df1eebb302fe6c295028f1866b507
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-01-14 05:00:38 +0100

    fix(playground): correct locale type

--- playground/app.vue
+++ playground/app.vue
@@ -5,18 +5,18 @@
     <fieldset>
       <div>
         <label for="input-en">English</label>
-        <input id="input-en" v-model="locale" type="radio" :value="Locale.EN" />
+        <input id="input-en" v-model="locale" type="radio" value="en" />
       </div>
       <div>
         <label for="input-de">German</label>
-        <input id="input-de" v-model="locale" type="radio" :value="Locale.DE" />
+        <input id="input-de" v-model="locale" type="radio" value="de" />
       </div>
     </fieldset>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Locale } from '../src/runtime/types'
+import type { Locale } from '../src/runtime/types'
 
 const locale = ref<Locale>('en')
 </script>
