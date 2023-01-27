607f5a8e97d8e3610b1afd10665b3349227226d0
Chingiz Mammadov <chingiz@Chingizs-MacBook-Pro.local>
2023-01-27 23:59:18 +0400

    Added Azerbaijani locale

---
+++ src/runtime/locale/az.ts
@@ -0,0 +1,20 @@
+import { LocaleStrings } from '../types'
+
+export default {
+  acceptAll: 'Hamısını qəbul et',
+  acceptNecessary: 'Zəruri olanları qəbul et',
+  barDescription:
+    'Biz öz kukilərimizdən və üçüncü tərəf kukilərindən istifadə edirik ki, sizə bu veb-saytı göstərə bilək və təklif etdiyimiz xidmətləri təkmilləşdirmək məqsədilə ondan necə istifadə etdiyinizi daha yaxşı başa düşək. Baxmağa davam etsəniz, kukiləri qəbul etdiyinizi hesab edirik.',
+  barTitle: 'Kukilər',
+  blockedIframe: 'Bunu görmək üçün funksional kukiləri aktiv edin',
+  close: 'Bağla',
+  declineAll: 'Hamısını rədd et',
+  functional: 'Funksional kukilər',
+  here: 'burada',
+  manageCookies: 'Kukiləri idarə et',
+  necessary: 'Zəruri kukilər',
+  optional: 'İstəyə bağlı kukilər',
+  none: 'heç biri',
+  save: 'Yadda saxla',
+  unsaved: 'Yadda saxlanmamış ayarlarınız var',
+} as LocaleStrings
