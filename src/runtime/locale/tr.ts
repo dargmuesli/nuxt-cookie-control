8dd647c6030911857976def4667f7eb1631783c2
Onur Dumangöz <40442783+onurdumangoz@users.noreply.github.com>
2023-01-29 19:41:08 +0300

    feat(i18n): add Turkish translation

---
+++ src/runtime/locale/tr.ts
@@ -0,0 +1,20 @@
+import { LocaleStrings } from '../types'
+
+export default {
+  acceptAll: 'Tümünü kabul et',
+  acceptNecessary: 'Sadece gerekliler',
+  barDescription:
+    'Bu web sitesindeki deneyiminizi iyileştirmek amacıyla kendi çerezlerimizi ve üçüncü taraf çerezleri kullanıyoruz. Siteyi kullanmaya devam ederseniz çerezleri kabul ettiğinizi kabul ederiz.',
+  barTitle: 'Çerezler',
+  blockedIframe: 'Bunu görmek için lütfen işlevsel çerezleri etkinleştirin',
+  close: 'Kapat',
+  declineAll: 'Tümünü reddet',
+  functional: 'İşlevsel çerezler',
+  here: 'here',
+  manageCookies: 'Çerezleri yönet',
+  necessary: 'Gerekli çerezler',
+  optional: 'İsteğe bağlı çerezler',
+  none: 'hiçbiri',
+  save: 'Kaydet',
+  unsaved: 'Kaydedilmemiş ayarlar mevcut',
+} as LocaleStrings
