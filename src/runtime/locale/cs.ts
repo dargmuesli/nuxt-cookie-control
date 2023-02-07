5db470ca5b8c1fd131be8a52f4c218762374f72f
Ondřej Misák <email@ondrejmisak.cz>
2023-02-07 12:33:11 +0100

    feat(locale): add Czech and Slovak locales

---
+++ src/runtime/locale/cs.ts
@@ -0,0 +1,20 @@
+import { LocaleStrings } from '../types'
+
+export default {
+  accept: 'Přijmout',
+  acceptAll: 'Přijmout vše',
+  bannerDescription:
+    'Používáme soubory cookie a cookie třetích stran, abychom mohli vše správně zobrazovat a lépe porozumět tomu, jak tento web používáte, s cílem zlepšit nabízené služby. Rozhodnutí lze kdykoli změnit pomocí tlačítka cookie, které se zobrazí po provedení výběru na tomto banneru.',
+  bannerTitle: 'Cookies',
+  close: 'Zavřít',
+  cookiesFunctional: 'Obslužné cookies',
+  cookiesNecessary: 'Nezbytné cookies',
+  cookiesOptional: 'Volitelné cookies',
+  decline: 'Zamítnout',
+  declineAll: 'Zamítnout vše',
+  here: 'zde',
+  iframeBlocked: 'Chcete-li ji zobrazit, povolte obslužné cookies',
+  manageCookies: 'Další informace a přizpůsobení',
+  save: 'Uložit',
+  settingsUnsaved: 'Máte neuložená nastavení',
+} as LocaleStrings
