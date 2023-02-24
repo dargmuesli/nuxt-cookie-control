9111d9cc02a5db4b307b9fee57ffebadd9de2027
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-02-24 17:26:58 +0100

    feat(i18n): adapt new locale format for Turkish translation

--- src/runtime/locale/tr.ts
+++ src/runtime/locale/tr.ts
@@ -1,20 +1,20 @@
 import { LocaleStrings } from '../types'
 
 export default {
+  accept: 'Kabul etmek',
   acceptAll: 'Tümünü kabul et',
-  acceptNecessary: 'Sadece gerekliler',
-  barDescription:
-    'Bu web sitesindeki deneyiminizi iyileştirmek amacıyla kendi çerezlerimizi ve üçüncü taraf çerezleri kullanıyoruz. Siteyi kullanmaya devam ederseniz çerezleri kabul ettiğinizi kabul ederiz.',
-  barTitle: 'Çerezler',
-  blockedIframe: 'Bunu görmek için lütfen işlevsel çerezleri etkinleştirin',
+  bannerDescription:
+    'Bu web sitesini doğru bir şekilde görüntüleyebilmek ve teklifimizi geliştirmek için bu web sitesinin nasıl kullanıldığını daha iyi anlayabilmek için kendi ve üçüncü taraf çerezlerimizi kullanıyoruz. Çerez kullanma izni ile ilgili karar, bu banner üzerinde bir seçim yapıldıktan sonra görünen çerez düğmesi aracılığıyla herhangi bir zamanda değiştirilebilir.',
+  bannerTitle: 'Çerezler',
   close: 'Kapat',
+  cookiesFunctional: 'İşlevsel çerezler',
+  cookiesNecessary: 'Gerekli çerezler',
+  cookiesOptional: 'İsteğe bağlı çerezler',
+  decline: 'Reddetmek',
   declineAll: 'Tümünü reddet',
-  functional: 'İşlevsel çerezler',
   here: 'here',
+  iframeBlocked: 'Bunu görmek için lütfen işlevsel çerezleri etkinleştirin',
   manageCookies: 'Çerezleri yönet',
-  necessary: 'Gerekli çerezler',
-  optional: 'İsteğe bağlı çerezler',
-  none: 'hiçbiri',
   save: 'Kaydet',
-  unsaved: 'Kaydedilmemiş ayarlar mevcut',
+  settingsUnsaved: 'Kaydedilmemiş ayarlar mevcut',
 } as LocaleStrings
