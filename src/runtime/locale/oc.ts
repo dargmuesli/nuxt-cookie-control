0bd74a644baac77103d6a48689fb2cc0cb5df98a
ensag-dev <59049879+ensag-dev@users.noreply.github.com>
2023-04-12 19:52:48 +0200

    Create oc.ts

---
+++ src/runtime/locale/oc.ts
@@ -0,0 +1,20 @@
+import { LocaleStrings } from '../types'
+
+export default {
+  accept: 'Acceptar',
+  acceptAll: 'Tot acceptar',
+  bannerDescription:
+    'Utilizam de cookies d’origina e de cookies tèrces. Aquestes cookies son destinats a vos ofrir una navegacion optimizada sus aqueste site web de nos donar un apercebut de son utilizacion, per melhorar los servicis que vos provesissèm. En contunhant vòstra navegacion, consideram qu’acceptatz l’usatge de cookies.',
+  bannerTitle: 'Cookies',
+  close: 'Tancar',
+  cookiesFunctional: 'Cookies foncionals',
+  cookiesNecessary: 'Los cookies obligatòris',
+  cookiesOptional: 'Los cookies opcionals',
+  decline: "Accèpti çò necessari",
+  declineAll: 'Tot refusar',
+  here: 'aquí',
+  iframeBlocked: 'Per veire aquò, mercés d’activar los cookies foncionals',
+  manageCookies: 'Gerir los cookies',
+  save: 'Salvagardar',
+  settingsUnsaved: 'Avètz de paramètres non salvagardats',
+} as LocaleStrings
