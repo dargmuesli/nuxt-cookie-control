b526977d62402baaa312411ca39397b0658487b5
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-07-26 11:29:16 +0200

    fix: export types (#112)
    * fix(module): import runtime types without relative path
    
    * fix(plugin): export app type

--- src/module.ts
+++ src/module.ts
@@ -60,7 +60,7 @@ export default defineNuxtModule<ModuleOptions>({
       filename: 'cookie-control-options.ts',
       write: true,
       getContents: () =>
-        `import { ModuleOptions } from '../../src/runtime/types'\n\nexport default ${JSON.stringify(
+        `import { ModuleOptions } from '#cookie-control/types'\n\nexport default ${JSON.stringify(
           moduleOptions,
           undefined,
           2,
