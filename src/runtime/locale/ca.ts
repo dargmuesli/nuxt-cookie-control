451452de8bb01fb41351a35b2d1b935b91371975
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-15 18:07:09 +0200

    chore(locale): format

--- src/runtime/locale/ca.ts
+++ src/runtime/locale/ca.ts
@@ -4,7 +4,7 @@ export default {
   accept: 'Acceptar',
   acceptAll: 'Acceptar-ho tot',
   bannerDescription:
-    'Utilitzem cookies pròpies i de tercers per tal de poder visualitzar correctament aquest lloc web i entendre millor com s\'utilitza, amb l\'objectiu de millorar els serveis que oferim. Una decisió sobre els permisos d\'ús de cookies es pot canviar en qualsevol moment mitjançant el botó de cookies que apareixerà després d\'haver fet una selecció en aquest bàner.',
+    "Utilitzem cookies pròpies i de tercers per tal de poder visualitzar correctament aquest lloc web i entendre millor com s'utilitza, amb l'objectiu de millorar els serveis que oferim. Una decisió sobre els permisos d'ús de cookies es pot canviar en qualsevol moment mitjançant el botó de cookies que apareixerà després d'haver fet una selecció en aquest bàner.",
   bannerTitle: 'Cookies',
   close: 'Tancar',
   cookiesFunctional: 'Cookies funcionals',
