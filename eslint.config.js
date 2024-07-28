f186e4fb3f83f5449f77822bcddc38e9c81a4bb2
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-07-29 01:59:01 +0200

    chore(eslint): enable typescript

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import prettierRecommended from 'eslint-plugin-prettier/recommended'
 export default createConfigForNuxt({
   features: {
     tooling: true,
+    typescript: true,
   },
   dirs: {
     src: ['./playground'],
