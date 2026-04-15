e91e24fd0074627d8e8b6a5dc83a362661b5f4a4
Tobias Jensen <bluebazze@gmail.com>
2026-04-15 12:21:57 +0200

    feat: add /kit export with addControlledCookie helper

---
+++ build.config.ts
@@ -0,0 +1,7 @@
+/**
+ * @see https://github.com/nuxt/module-builder?tab=readme-ov-file#buildconfigts-optional
+ * Should just be extending and not overriding the default config
+ */
+export default {
+  entries: ['src/kit'],
+}
---
+++ playground/modules/auth.ts
@@ -0,0 +1,18 @@
+import { defineNuxtModule, hasNuxtModule } from '@nuxt/kit'
+import { addCookie, CookieType } from '@dargmuesli/nuxt-cookie-control/kit'
+
+export default defineNuxtModule({
+  setup() {
+    if (hasNuxtModule('@dargmuesli/nuxt-cookie-control')) {
+      addCookie(
+        {
+          id: 'module-added-cookie',
+          name: 'Module Added Cookie',
+          description: 'This cookie was added from the module.',
+          targetCookieIds: ['_module_added_cookie'],
+        },
+        CookieType.OPTIONAL,
+      )
+    }
+  },
+})
